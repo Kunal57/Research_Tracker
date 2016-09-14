eee5d76c6fa1da4d70372eb94616e022c495708d
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-09-14 17:50:22 -0500

    add is_professor, is_student

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def current_user
      User.find(session[:user_id])
   end
+
+  def is_professor?
+  	Professor.all.include?(current_user)
+  end
+
+  def is_student?
+  	Student.all.include?(current_user)
+  end
 end
