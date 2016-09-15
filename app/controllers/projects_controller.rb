4377f4c2e9f79d8d82b2dcdaf471b45d975b86b0
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-15 11:00:30 -0500

    if professor, logs include hours worked and budget remaining. if student, total hours worked on project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,5 +6,8 @@ class ProjectsController < ApplicationController
 
 	def show
 		@project = Project.find(params[:id])
+		if is_student?
+			@student_total_hours = current_user.hours_per_project(@project.id)
+		end
 	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -10,4 +10,12 @@ class Project < ActiveRecord::Base
   def complete?
   	self.status == "complete"
   end
+
+  def remaining_time
+  	total_worked = 0
+  	self.records.each do |record|
+  		total_worked += record.hours_worked
+  	end
+  	self.budget - total_worked 
+  end
 end
