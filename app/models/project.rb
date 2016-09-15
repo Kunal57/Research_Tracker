2fa5203ff3ef6961f310ef02ac2ec7e2978d77d2
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-15 09:36:42 -0500

    add method for calculating total hours worked, current / completed project status, update the default hours worked to be zero, display on profile page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,11 @@ class Project < ActiveRecord::Base
   has_many :records
   has_many :students, through: :records
 
+  def current? 
+  	self.status == "active"
+  end
+
+  def complete?
+  	self.status == "complete"
+  end
 end
