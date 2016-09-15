1d04208265bdd4bf52b600b21608e4ea6b78749a
William Huebler <hueblerw@gmail.com>
2016-09-15 06:54:20 -0500

    add the projects routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :projects
 
   # Example resource route with options:
   #   resources :products do
