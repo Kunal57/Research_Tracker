5fbdbf698b83fb8b78ed04fb677f0dbcea745ae1
William Huebler <hueblerw@gmail.com>
2016-09-15 10:32:33 -0500

    Fixed errors preventing reports from being created

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,11 +15,14 @@ class ProjectsController < ApplicationController
 	  	new_project = Project.new(title: params[:project][:title], hypothesis: params[:project][:hypothesis], summary: params[:project][:summary], time_budget: params[:project][:time_budget], professor_id: 1)
 	  	if new_project.save
 	  		p params
-	  		# Create a report for each new array of students.
+	  		# Create a record for each new array of students.
 	  		i = 1
 	  		while i < params[:students][:ids].length + 1
-	  			if params[:students][:ids][i.to_s] == 1
-	  				Report.create(project_id: new_project.id, student_id: i)
+	  			if params[:students][:ids][i.to_s] == "1"
+	  				record = Record.new(project_id: new_project.id, student_id: i)
+	  				if !record.save
+	  					@errors = record.errors.full_messages
+	  				end
 	  			end
 	  			i += 1
 	  		end
