6b603ae92893b23987445bcb3a28b186dbaa8eae
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-15 10:29:08 -0500

    project profile displays hypothesis and summary

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :professors, only: [:show, :new, :create]
   resources :students, only: [:show, :new, :create]
-  resources :projects
+  resources :projects, only: [:new, :index, :create, :show]
 
 
 
