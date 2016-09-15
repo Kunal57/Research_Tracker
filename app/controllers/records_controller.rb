480454afa257e706379beddc0e8487eab571df3b
William Huebler <hueblerw@gmail.com>
2016-09-15 18:02:59 -0500

    Create rough draft of the create record controller route

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,6 +2,16 @@ class RecordsController < ActionController::Base
 
 	def create
 		#Create the new record.
+		p "----------------------------------------------------"
+		p params
+		record = Record.new(params.permit(:hours_worked, :observations, :project_id))
+		record.student_id = current_user.id
+		p record
+		p "----------------------------------------------------"
+		if record.save
+			@errors = record.errors.full_messages
+		end
+		redirect_to Project.find(params[:record][:project_id])
 	end
 
 end
