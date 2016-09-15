979136dbccb8017188f38cabb83190f5e7a1ecf6
Ben Newman <benjaminjnewman@gmail.com>
2016-09-15 15:28:36 -0500

    kept old funny seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,19 @@ Professor.create(name: "Matt Baker", email: "matt@madzcintiztz.com", password: "
 
 Student.create(name: "Duke", email: "duke@duke.com", password: "password")
 
+Project.create(title: "Mad Zcienz", hypothesis: "cool shit will happen", summary: "do experiments, yo", time_budget: 400, professor_id: 2)
+ 
+Record.create(student_id: 1, project_id: 1)
+ 
+# Active Project
+Project.create(title: "Why does the sun shine?", hypothesis: "The Sun is a mass of incandescent gas", summary: "a gigantic nuclear furnace", time_budget: 400, professor_id: 2, status: "active")
+ 
+# Completed Project
+Project.create(title: "SCIENCE!", hypothesis: "it's poetry in motion", summary: "She blinded me, with SCIENCE!", time_budget: 10000, professor_id: 2, status: "complete")
+ 
+# Rejected Project
+Project.create(title: "Weird Science", hypothesis: "cool shit will happen", summary: "do experiments, yo", time_budget: 400, professor_id: 2, status: "rejected")
+
 
 10.times do
   Professor.create!(email: Faker::Internet.email,
