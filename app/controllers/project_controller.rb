b564881d38b9e2bee268795ef99019ca72596180
William Huebler <hueblerw@gmail.com>
2016-09-15 07:05:59 -0500

    Create a first draft of the create controller for projects

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -10,4 +10,21 @@ class ApplicationController < ActionController::Base
   	end
   end
 
+  def create
+  	if true #current_professor
+	  	new_project = Project.new(title: params[:project][:title], hypothesis: params[:project][:hypothesis], summary: params[:project][:summary], time_budget: params[:project][:time_budget])
+	  	if new_project.save
+	  		p params
+	  		# Create a report for each new array of students.
+	  		# new_report = Report.new()
+	  		redirect_to current_professor
+	  	else
+	  		#display the error messages.
+	  		@errors = new_project.errors.full_messages
+	  	end
+	else
+		redirect_to "/"
+	end
+  end
+
 end
