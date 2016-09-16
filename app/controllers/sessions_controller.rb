ed80b2a1ba56d416a106f659f76db5ea7d31780d
Ben Newman <email": null>
2016-09-16 13:19:12 -0500

    redirect admin to acct management on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,20 @@ class SessionsController < ApplicationController
   def create
     @student = Student.find_by(email: params[:email])
     @professor = Professor.find_by(email: params[:email])
-    if @student && @student.authenticate(params[:password])
-      session[:student_id] = @student.id
-      redirect_to @student
-    elsif @professor && @professor.authenticate(params[:password])
-      session[:professor_id] = @professor.id
-      redirect_to @professor
-    else
-      flash[:notice] = "Invalid email/password combination, please try again"
-      render 'new'
-    end
+      if @student && @student.authenticate(params[:password])
+        session[:student_id] = @student.id
+        redirect_to @student
+      elsif @professor && @professor.authenticate(params[:password])
+        session[:professor_id] = @professor.id
+        if is_admin?
+          redirect_to '/admin/secr1t'
+        else
+          redirect_to @professor
+        end
+      else
+        flash[:notice] = "Invalid email/password combination, please try again"
+        render 'new'
+      end
   end
 
   def destroy
